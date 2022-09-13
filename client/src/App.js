7342dcf2e594400d17c3c4306f6641f4445d21b3
Petr Studeny <petr.studeny@unicorn.com>
2022-09-13 10:37:21 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const classroom = {
+  name: "Septima"
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Classroom {classroom.name}</h1>
+    </div>
+  );
+}
+
+export default App;
