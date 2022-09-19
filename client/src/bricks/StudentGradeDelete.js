a6fe38acfd3f6280e42fd8d04b56c7aabafc192b
Petr Studeny <petr.studeny@unicorn.com>
2022-09-19 11:30:14 +0200

    block 10 - delete grade, confirmation

--- client/src/bricks/StudentGradeDelete.js
+++ client/src/bricks/StudentGradeDelete.js
@@ -10,8 +10,8 @@ function StudentGradeDelete({grade, onDelete, onError}) {
     })
 
     const handleDelete = async () => {
-        if (deleteGradeCall.state = "pending")
-            return
+        // if (deleteGradeCall.state = "pending")
+        //     return
 
         setDeleteGradeCall({state: "pending"})
 
@@ -47,11 +47,13 @@ function StudentGradeDelete({grade, onDelete, onError}) {
             confirmText="Smazat"
             onConfirm={handleDelete}
         >
-            <Icon
-                path={mdiTrashCanOutline}
-                style={{cursor: 'pointer', color: 'red'}}
-                size={0.8}
-            ></Icon>
+            <div>
+                <Icon
+                    path={mdiTrashCanOutline}
+                    style={{cursor: 'pointer', color: 'red'}}
+                    size={0.8}
+                ></Icon>
+            </div>
         </Confirmation>
     )
 }
